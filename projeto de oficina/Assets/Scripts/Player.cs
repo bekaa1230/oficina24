75b10bb5a7130a74be3a6a045c031d519829490e
bekaa1230 <vitoriarebeka606@gmail.com>
2024-08-30 18:17:14 -0300

    FASE 1 salve 5 ead 2

--- projeto de oficina/Assets/Scripts/Player.cs
+++ projeto de oficina/Assets/Scripts/Player.cs
@@ -11,12 +11,14 @@ public class Player : MonoBehaviour
    public bool doubleJump;
    
    private Rigidbody2D rig;
+   private Animator anim;
 
    
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,6 +32,23 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
+        
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool ("walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool ("walk", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+
+        if(Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool ("walk", false);
+        }
 
     }
 
@@ -40,14 +59,15 @@ public class Player : MonoBehaviour
         {
             if(isJumping)
             {
-                rig.AddForce(new Vector3(0f, JumpForce), ForceMode2D.Impulse);
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
+                anim.SetBool ("jump", true);
             }
             else
             {
                 if(doubleJump)
                 {
-                    rig.AddForce(new Vector3(0f, JumpForce), ForceMode2D.Impulse);
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
                     doubleJump = false;
                 }
 
@@ -64,6 +84,7 @@ public class Player : MonoBehaviour
         if(collision.gameObject.layer == 8)
         {
             isJumping = false;
+             anim.SetBool ("jump", false);
 
         }
 
